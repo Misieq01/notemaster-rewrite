5b400988cbe139811ec8de814305c90a92e24633
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2020-03-14 12:21:24 +0100

    Fixed closing menus in navigation

--- src/Application/Navigation/Buttons/MenuButton.js
+++ src/Application/Navigation/Buttons/MenuButton.js
@@ -1,37 +1,35 @@
 import React, { useState, useRef } from "react";
 import Portal from "../../Components/ReactPortal";
-import {AnimatePresence} from 'framer-motion'
-
+import { AnimatePresence } from "framer-motion";
 
 const Button = ({ svg, buttonTitle, MenuContent, ...props }) => {
   // Manage display of menu component
   const [portalActive, setPortalActive] = useState(false);
 
-  const Mount = () => {
-    setPortalActive(true);
-  };
+  // This state allow closing menu when clicking on button
+  // We just needed extra state to manage display which would not be connected to portal
+  const [helpingState,setHelp] = useState(false)
 
   const UnMount = () => {
-    setPortalActive(false);
+      setPortalActive(false);
   };
+
+  const ClickHandler = () =>{
+    if(!helpingState){
+      setPortalActive(true)
+      setHelp(true)
+    }else if(helpingState){
+      setPortalActive(false)
+      setHelp(false);
+
+    }
+  }
 
   // Reference to buttom DOM element
   const ButtonRef = useRef();
-
-  const ClickHandler = () => {
-    if (!portalActive) {
-      Mount();
-    }
-  };
-
   return (
     <>
-      <button
-        className="navigation__wrapper--button"
-        ref={ButtonRef}
-        title={buttonTitle}
-        onClick={ClickHandler}
-      >
+      <button className="navigation__wrapper--button" ref={ButtonRef} title={buttonTitle} onClick={ClickHandler}>
         <img alt="icon" className="navigation__wrapper--icon" src={svg} />
       </button>
       <AnimatePresence>
