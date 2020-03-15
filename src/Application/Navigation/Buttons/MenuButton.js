cb31acce84ea71529559f2a3910fb7449a7be229
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2020-03-15 17:10:01 +0100

    quick refactor in menuButton

--- src/Application/Navigation/Buttons/MenuButton.js
+++ src/Application/Navigation/Buttons/MenuButton.js
@@ -6,30 +6,12 @@ const Button = ({ svg, buttonTitle, MenuContent, ...props }) => {
   // Manage display of menu component
   const [portalActive, setPortalActive] = useState(false);
 
-  // This state allow closing menu when clicking on button
-  // We just needed extra state to manage display which would not be connected to portal
-  const [helpingState, setHelp] = useState(false);
-
   const UnMount = () => {
-    setPortalActive(false);
-    setTimeout(()=>setHelp(false),0)
+    setTimeout(()=>setPortalActive(!portalActive),0)
   };
 
-  console.log("portal", portalActive);
-  console.log("help", helpingState);
-
   const ClickHandler = () => {
-    if (!helpingState) {
-      setPortalActive(true);
-      setHelp(true);
-    } else if (helpingState) {
-      setPortalActive(false);
-      setHelp(false);
-    } else if (helpingState && !portalActive) {
-      console.log("fired");
-      setPortalActive(true);
-      setHelp(true);
-    }
+    setPortalActive(!portalActive)
   };
 
   // Reference to buttom DOM element
