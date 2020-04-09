6b7eedc596206eb10d505adde5b2d92494e5a282
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2020-04-09 23:37:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
   app.use(express.static(path.join(__dirname, "client/build")));
   
   app.get("*", (req, res) => {
-    res.sendFile(path.join((__dirname + "client/build/index.html")));
+    res.sendFile(path.join((__dirname = "client/build/index.html")));
   });
   
 app.listen(PORT, () => {
